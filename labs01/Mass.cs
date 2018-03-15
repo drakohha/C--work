0b4b26161cb38de2359ae9680e706c9f16744360
drakohha <drakohha@tut.by>
2018-03-15 11:43:51 +0300

    labs01 classwork

---
+++ labs01/Mass.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab01
+{
+    class Mass
+    {
+        public static int[] A = new int[5];
+        public static double[,] B = new double[3, 4];
+
+        public void SetValueA()
+        {
+            string s;
+            for (int i = 0; i < 5; i++)
+            {
+                s = Console.ReadLine();
+                A[i] = Convert.ToInt32(s);
+            }
+        }
+
+         public static Random rnd = new Random();
+
+        public static int rows = B.GetUpperBound(0) + 1;
+        public static int columns = B.Length / rows;
+        public void SetValueB()
+        {
+            
+            
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                   
+
+
+                    B[i, j] = rnd.NextDouble() * 100;
+                }
+                
+            }
+        }
+
+
+        public void PrintA()
+        {
+            foreach (int i in A)
+            {
+                Console.Write(i + " ");
+            }
+
+        }
+
+        public void PrintB()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{B[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+    }
+}
---
+++ labs01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int flag = 0;
+            Mass ms = new Mass();
+            while (true)
+            {
+
+                Console.WriteLine("Введите числа  для массива А= \n");
+                ms.SetValueA();
+                Console.WriteLine("Генерируються числоа для массива В! \n");
+                ms.SetValueB();
+                Console.WriteLine("Вывод на экран массив А=\n");
+                ms.PrintA();
+                Console.WriteLine("\n Вывод на экран массив B=\n");
+                ms.PrintB();
+
+                Console.WriteLine("\n Закончить - 0 повторить - 1\n");
+                flag = Convert.ToInt32( Console.ReadLine());
+                if (flag == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+          
+
+          
+
+
+           
+        }
+    }
+}
