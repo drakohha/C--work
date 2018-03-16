01f5430b103af35fcd173c737deb3a819d54bb6a
drakohha <drakohha@tut.by>
2018-03-16 11:53:57 +0300

    update on class

--- labs01/Mass.cs
+++ labs01/Mass.cs
@@ -10,6 +10,216 @@ namespace lab01
     {
         public static int[] A = new int[5];
         public static double[,] B = new double[3, 4];
+        public static int[] M = new int[6];
+        public static int[] N = new int[6];
+        public static int[] MN= new int[6] ;
+        public static int[,] TT = new int[5, 5];
+
+        public static int rows = B.GetUpperBound(0) + 1;
+        public static int columns = B.Length / rows;
+
+        public static int rows_2 = TT.GetUpperBound(0)+1;
+        public static int columns_2 = TT.Length / rows_2;
+
+        public static int Max = 0;
+        public static int Min = 100;
+        public static int sum = 0;
+        public static double sum_2 = 0.0;
+        public static int pr = 0;
+        public static double pr_2 = 0.0;
+        public static double Max_2 = 0;
+        public static double Min_2 = 100;
+
+
+        public int[] GetmassM()
+        {
+            return M;
+        }
+        public int[] GetmassA()
+        {
+            return A;
+        }
+        public double[,] GetmassB()
+        {
+            return B;
+        }
+
+        public int[] GetmassN()
+        {
+            return N;
+        }
+        public int[,] GetmassTT()
+        {
+            return TT;
+        }
+
+        public int[] GetmassMN()
+        {
+            return MN;
+        }
+
+        public int FindMax( int[] X)
+        {
+            Max = X.Max();
+            return Max;
+        }
+
+
+        public int Summ(int[] X)
+        {
+            sum = 0;
+            foreach(int i in X)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+        public double Summ_2(double[,] X)
+        {
+            sum_2 = 0.0;
+            foreach (double i in X)
+            {
+                sum_2 += i;
+            }
+            return sum_2;
+        }
+
+
+        public int FindMax_2(int[,] Z)
+        {
+            Max = 0;
+            foreach(int i in Z)
+            {
+                if (i > Max)
+                {
+                    Max = i;
+                }
+                
+
+            }
+            return Max;
+        }
+
+
+        public double FindMax_3(double[,] Z)
+        {
+            Max_2 = 0.0;
+            foreach (double i in Z)
+            {
+                if (i > Max_2)
+                {
+                    Max_2 = i;
+                }
+
+
+            }
+            return Max_2;
+        }
+
+
+        public int FindMin(int [] Z)
+        {
+            Min = Z.Min();
+            return Min;
+        }
+        public double FindMin_2(double [,] Z)
+        {
+            Min_2 = 100.0;
+            foreach (double i in Z)
+            {
+                if (i < Min_2)
+                {
+                    Min_2 = i;
+                }
+
+
+            }
+            return Min_2;
+        }
+
+
+        public void SetValueTT()
+        {
+
+            for (int i = 0; i < rows_2; i++)
+            {
+                for (int j = 0; j < columns_2; j++)
+                {
+
+
+
+                    TT[i, j] = rnd.Next(-100,100);
+                }
+
+            }
+        }
+
+        public void PrintTT() {
+            for (int i = 0; i < rows_2; i++)
+            {
+                for (int j = 0; j < columns_2; j++)
+                {
+                    Console.Write($"{TT[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+
+        public void SetValueM()
+        {
+            string s;
+            for (int i = 0; i < 6; i++)
+            {
+                s = Console.ReadLine();
+                M[i] = Convert.ToInt32(s);
+            }
+        }
+
+        public void SetValueN()
+        {
+            string s;
+            for (int i = 0; i < 6; i++)
+            {
+                s = Console.ReadLine();
+                N[i] = Convert.ToInt32(s);
+            }
+        }
+
+        public void SetValueMN()
+        {
+            int k = 0;
+            foreach(int i in M)
+            {
+                foreach(int j in N)
+                {
+                    if (i == j)
+                    {
+                        MN[k] = i;
+                        k++;
+                        break;
+
+                    }
+                }
+            }
+        }
+
+        public void PrintMN()
+        {
+            foreach (int i in MN)
+            {
+                if (i != 0)
+                {
+                    Console.Write(i + " ");
+                }
+               
+            }
+
+
+        }
+
 
         public void SetValueA()
         {
@@ -23,8 +233,7 @@ namespace lab01
 
          public static Random rnd = new Random();
 
-        public static int rows = B.GetUpperBound(0) + 1;
-        public static int columns = B.Length / rows;
+        
         public void SetValueB()
         {
             
@@ -65,6 +274,83 @@ namespace lab01
             }
 
         }
+
+        public int FindIndexMin(int[,] Z)
+        {
+            int min_4=0;
+            int min = 100;
+            int kkk = 0;
+
+            foreach(int i in Z)
+            {
+                kkk++;
+                if (i < min)
+                {
+                    min = i;
+                    min_4 = kkk;
+                }
+            }
+
+            return min_4;
+        }
+
+        public int FindIndexMax(int[,] Z)
+        {
+            int max_4 = 0;
+            int max = 100;
+            int kkk = 0;
+
+            foreach (int i in Z)
+            {
+                kkk++;
+                if (i > max)
+                {
+                    max = i;
+                    max_4 = kkk;
+                }
+            }
+
+            return max_4;
+        }
+
+
+        public int summ_3(int min_4 , int max_4,int[,] Z)
+        {
+            sum = 0;
+            int kk =0;
+            foreach(int i in Z)
+            {
+                kk++;
+                if(kk>=min_4 && kk <= max_4)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+
+
+       
+        public int proiz(int[] Z)
+        {
+            pr = 1;
+            foreach(int i in Z)
+            {
+                pr *= i;
+            }
+            return pr;
+        }
+
+        public double proiz_2(double[,] Z)
+        {
+            pr_2 = 1;
+            foreach (double i in Z)
+            {
+                pr_2 *= i;
+            }
+            return pr_2;
+        }
+
 
     }
 }
--- labs01/Program.cs
+++ labs01/Program.cs
@@ -15,14 +15,85 @@ namespace lab01
             while (true)
             {
 
+                Console.WriteLine("\nЗадание номер 1\n");
+
                 Console.WriteLine("Введите числа  для массива А= \n");
                 ms.SetValueA();
-                Console.WriteLine("Генерируються числоа для массива В! \n");
+                Console.WriteLine("Генерируються числа для массива В! \n");
                 ms.SetValueB();
                 Console.WriteLine("Вывод на экран массив А=\n");
                 ms.PrintA();
                 Console.WriteLine("\n Вывод на экран массив B=\n");
                 ms.PrintB();
+
+
+                Console.WriteLine("\nЗадание номер 2\n");
+
+                Console.WriteLine("\nПоиск минимального элемента массива А и В=\n");
+                int min = ms.FindMin(ms.GetmassA());
+                Console.WriteLine("\n минимального элемент массива А = \n");
+                Console.WriteLine(min);
+
+                double min_2 = ms.FindMin_2(ms.GetmassB());
+                Console.WriteLine("\n минимального элемент массива B = \n");
+                Console.WriteLine(min_2);
+
+                Console.WriteLine("\nПоиск максимального элемента массива А и В=\n");
+                int max= ms.FindMax(ms.GetmassA());
+                Console.WriteLine("\nмаксимальный элемент массива А = \n");
+                Console.WriteLine(max);
+
+                double max_2 = ms.FindMax_3(ms.GetmassB());
+                Console.WriteLine("\nмаксимальный элемент массива B = \n");
+                Console.WriteLine(max_2);
+
+                Console.WriteLine("\nПоиск общей суммы элементов в массивах А и В=\n");
+
+                int sum = ms.Summ(ms.GetmassA());
+                double sum_2 = ms.Summ_2(ms.GetmassB());
+                Console.WriteLine("\nобщея суммa элементов в массиве А=\n");
+                Console.WriteLine(sum);
+                Console.WriteLine("\nобщея суммa элементов в массиве B=\n");
+                Console.WriteLine(sum_2);
+
+                Console.WriteLine("\nПоиск общего произведения элементов в массивах А и В=\n");
+                int pr = 0;
+                double pr_2 = 0.0;
+
+                pr = ms.proiz(ms.GetmassA());
+                pr_2 = ms.proiz_2(ms.GetmassB());
+                Console.WriteLine("\nобщее произведение элементов в массиве А=\n");
+                Console.WriteLine(pr);
+                Console.WriteLine("\nобщее произведение элементов в массиве B=\n");
+                Console.WriteLine(pr_2);
+
+
+                Console.WriteLine("\nПоиск  суммы четных элементов  массива А =\n");
+
+                Console.WriteLine("\nПоиск  суммы нечетных элементов в столбцах массива В =\n");
+
+                Console.WriteLine("\nЗадание номер 3\n");
+                Console.WriteLine("Введите числа  для массива M= \n");
+                ms.SetValueM();
+                Console.WriteLine("Введите числа  для массива N= \n");
+                ms.SetValueN();
+                Console.WriteLine("Генерируються  массив MN! \n");
+                ms.SetValueMN();
+                Console.WriteLine("Вывод на экран массив MN=\n");
+                ms.PrintMN();
+
+                Console.WriteLine("\nЗадание номер 4\n");
+
+                Console.WriteLine("\n Генерация массива 5х5 с случайными числами \n");
+                ms.SetValueTT();
+                ms.PrintTT();
+                Console.WriteLine("\nПоиск общей суммы элементов между мин и мах елементами =\n");
+                min = ms.FindIndexMin(ms.GetmassTT());
+                max = ms.FindIndexMax(ms.GetmassTT());
+                int summ_3 = ms.summ_3(min , max , ms.GetmassTT());
+                Console.WriteLine(summ_3);
+
+
 
                 Console.WriteLine("\n Закончить - 0 повторить - 1\n");
                 flag = Convert.ToInt32( Console.ReadLine());
