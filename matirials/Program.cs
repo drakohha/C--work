a7716091c8502c0331f265a0d94a24011bebd366
drakohha <drakohha@tut.by>
2018-03-23 09:10:39 +0300

    new fails

---
+++ matirials/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using itstep.overloading;
+
+
+namespace EntryPointOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 6;
+            int y = 8;
+            y = y + x;
+            int z;
+            if (x > y)
+            {
+                z = x + y;
+            }
+            UserU u1 = new UserU() { age = 20, fio = "Иванов" };
+            UserU u2 = new UserU() { age = 18, fio = "Петров" };
+            UserU u3;
+            u3 = u1 + u2;
+            u3 = u1 + 8;
+            u3 += 6;
+            u3 += u1;
+
+            z = u3;
+
+
+        }
+    }
+}
---
+++ matirials/UserU.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace itstep
+{
+    namespace overloading{
+ 
+
+         public   class UserU
+            {
+                public int age;
+                public string fio;
+                public static UserU operator +(UserU x, UserU y)
+                {
+                    UserU tempOb = new UserU();
+                    tempOb.fio = "not defined";
+                    tempOb.age = (x.age + y.age) / 2;
+                    return tempOb;
+                }
+                /* public static UserU operator +(UserU x, int y)
+                 {
+                     x.age += y;
+                     return x;
+                 }*/
+                public static UserU operator +(UserU x, int y)
+                {
+                    UserU tempOb = new UserU();
+                    tempOb.fio = "not defined";
+                    tempOb.age = x.age + y;
+                    return tempOb;
+                }
+                public static implicit operator int(UserU x)
+                {
+
+                    return x.age;
+                }
+            }
+        }
+
+    
+}
