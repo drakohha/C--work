8fedc613dcc004163905ffa6ee6b6248c4dacbc4
drakohha <drakohha@tut.by>
2018-03-27 10:57:50 +0300

    Full complieat

--- lab05/Calc_kalendar.cs
+++ lab05/Calc_kalendar.cs
@@ -88,5 +88,228 @@ namespace lab05
         }
 
 
+
+        public static int operator - (Calc_kalendar k1, Calc_kalendar k2)
+        {
+            int rezult = 0;
+            int rez_ye = 0;
+            int rez_mon = 0;
+            int rez_dey = 0;
+
+
+
+            rez_ye = (k1.year - k2.year);
+            if (rez_ye < 0)
+            {
+               rez_ye= rez_ye * (-1);
+            }
+            rezult = rezult + rez_ye * 356;
+
+            rez_mon = (k1.month - k2.month);
+            if (rez_mon < 0)
+            {
+                rez_mon = rez_mon * (-1);
+            }
+            else if (rez_mon == 0)
+            {
+                rez_mon = 0;
+            }
+            rezult += rez_mon * 31;
+
+            rez_dey = k1.dday - k2.dday;
+            if (rez_dey < 0)
+            {
+                rez_dey = rez_dey * (-1);
+            }
+            rezult += rez_dey;
+            
+
+            return rezult; 
+        }
+
+
+        public static bool operator !=(Calc_kalendar k1, Calc_kalendar k2)
+        {
+            bool rez = false;
+            if (k1.dday != k2.dday || k1.month != k2.month || k1.year != k2.year)
+            {
+                rez = true;
+            }
+            else
+            {
+                rez = false;
+            }
+            return rez;
+        }
+
+        public static bool operator==(Calc_kalendar k1, Calc_kalendar k2)
+        {
+            bool rez=false;
+            if(k1.dday==k2.dday && k1.month==k2.month && k1.year == k2.year)
+            {
+                rez = true;
+            }
+            else
+            {
+                rez = false;
+            }
+            return rez;
+        }
+
+        /*
+        public static Calc_kalendar operator +(Calc_kalendar k1, int data)
+        {
+            int d = 0;
+            d = k1.dday + data;
+            int m = 0;
+            if (d > 31)
+            {
+                k1.month+= d / 31;
+                k1.dday = d % 31;
+
+                if (k1.month > 12)
+                {
+                    m = k1.month % 12;
+                    k1.year += m;
+                    k1.month = k1.month % 12;
+                }
+
+            }
+            else
+            {
+                k1.dday = d;
+            }
+
+
+            return k1;
+        }
+        */
+
+        public static Calc_kalendar operator +(Calc_kalendar k1, int data)
+        {
+            try
+            {
+                MyExc ex = new MyExc();
+                while (true)
+                {
+                    if (data>0 && data < 31)
+                    {
+                        k1.dday += data;
+                        if (k1.dday > 31)
+                        {
+                            k1.month++;
+                            k1.dday = k1.dday - 31;
+                        }
+                        break;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Error enter!");
+                        break;
+                    }
+                }
+            }
+            catch (MyExc ex)
+            {
+                if (data <= 0 || data > 31)
+                {
+                    Console.WriteLine(ex.Message);
+                    data=Convert.ToInt32(Console.ReadLine()); 
+                }
+
+
+            }
+            return k1;
+        }
+        public static Calc_kalendar operator +(Calc_kalendar k1, string s)
+        {
+
+            try
+            {
+                int data = 0;
+                data = Convert.ToInt32(s);
+                MyExc ex = new MyExc();
+                while (true)
+                {
+                    if(data<1 && data > 4)
+                    {
+                        Console.WriteLine("Error");
+                        break;
+                    }
+                    else
+                    {
+                        k1.dday += data * 7;
+                        if (k1.dday > 31)
+                        {
+                            k1.month++;
+                            k1.dday = k1.dday - 31;
+                        }
+                        break;
+                    }
+                }
+
+
+            }
+            catch (MyExc ex)
+            {
+                int data = 0;
+                data = Convert.ToInt32(s);
+                if (data <= 0 || data > 4)
+                {
+                    Console.WriteLine(ex.Message);
+                    data = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            return k1;
+        }
+
+
+        public static Calc_kalendar operator ++(Calc_kalendar k1)
+        {
+            k1.dday++;
+            if (k1.dday > 31)
+            {
+                k1.dday -= 31;
+                k1.month++;
+                if (k1.month > 12)
+                {
+                    k1.month -= 12;
+                    k1.year++;
+                }
+            }
+            return k1;
+        }
+
+        public static Calc_kalendar operator --(Calc_kalendar k1)
+        {
+            k1.dday--;
+            if (k1.dday <= 0)
+            {
+                k1.dday = 31;
+                k1.month--;
+                if (k1.month <= 0)
+                {
+                    k1.month = 12;
+                    k1.year--;
+                }
+            }
+            return k1;
+        }
+
+        public  DateTime Transeform (Calc_kalendar k1)
+        {
+            DateTime t1=new DateTime();
+            t1.AddDays(k1.dday);
+            t1.AddMonths(k1.month);
+            t1.AddYears(k1.year);
+
+
+
+
+            return t1;
+        }
+
+
     }
 }
--- lab05/Program.cs
+++ lab05/Program.cs
@@ -10,11 +10,23 @@ namespace lab05
     {
         static void Main(string[] args)
         {
-
+            int data = 0;
             Calc_kalendar A = new Calc_kalendar(12, 02, 2006);
             A.exc();
             A.ToConsole();
+            Calc_kalendar B = new Calc_kalendar(24, 07, 2002);
+            B.ToConsole();
+             data=A - B;
+            Console.WriteLine("Разница между двумя датами {0} дней",data);
 
+            A = A + data;
+            A = A + "2";
+
+            A++;
+            B--;
+            DateTime t1;
+            t1 = A.Transeform(A);
+            Console.WriteLine(t1);
 
             Console.ReadLine();
         }
