fa5e1429d85d788aed318063aa7ab73c85c8f04f
drakohha <drakohha@tut.by>
2018-04-11 11:56:57 +0300

    new

---
+++ lab13/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+using System.Xml.Linq;
+
+namespace lab14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserElement ls = new UserElement();
+
+            //XDocument xDoc = XDocument.Load("XMLlist.xml");
+
+            //xDoc.Descendants();
+            //Console.WriteLine(xDoc.Descendants());
+
+
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("XMLlist.xml");
+            // получим корневой элемент
+            XmlElement xRoot = xDoc.DocumentElement;
+            // обход всех узлов в корневом элементе
+            foreach (XmlNode xnode in xRoot)
+            {
+                // получаем атрибут name
+                if (xnode.Attributes.Count > 0)
+                {
+                    XmlNode attr = xnode.Attributes.GetNamedItem("id");
+                    if (attr != null)
+                        Console.WriteLine(attr.Value);
+                }
+                // обходим все дочерние узлы элемента user
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    // если узел - company
+                    if (childnode.Name == "fio")
+                    {
+                        Console.WriteLine("fio: {0}", childnode.InnerText);
+                    }
+                    // если узел age
+                    if (childnode.Name == "phone")
+                    {
+                        Console.WriteLine("phone: {0}", childnode.InnerText);
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+
+
+        }
+    }
+}
---
+++ lab13/UserElement.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab14
+{
+    class UserElement
+    {
+        public string department;
+        public string fio;
+        public DateTime DateBegin;
+        public string phone;
+
+        public List<UserElement> li= new List<UserElement>();
+
+        public void Addelem(string dep,string fio,string phone)
+        {
+            UserElement u= new UserElement();
+            u.DateBegin = DateTime.Now;
+            u.department = dep;
+            u.fio = fio;
+            u.phone = phone;
+            li.Add(u);
+        }
+
+        public UserElement()
+        {
+
+        }
+
+        public void GetAll()
+        {
+            foreach(var i in li)
+            {
+                Console.WriteLine(i.fio+' '+i.department +' ' + i.phone + ' ' + i.DateBegin);
+
+
+            }
+        }
+
+
+
+    }
+}
