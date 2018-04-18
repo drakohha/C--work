4722e7c00ab75271a3a43ff0f9e7283a5c5c1904
drakohha <drakohha@tut.by>
2018-04-18 09:39:49 +0300

    full

--- lab13/Program.cs
+++ lab13/Program.cs
@@ -13,44 +13,9 @@ namespace lab14
     {
         static void Main(string[] args)
         {
-            UserElement ls = new UserElement();
-
-            //XDocument xDoc = XDocument.Load("XMLlist.xml");
-
-            //xDoc.Descendants();
-            //Console.WriteLine(xDoc.Descendants());
-
-
-            XmlDocument xDoc = new XmlDocument();
-            xDoc.Load("XMLlist.xml");
-            // получим корневой элемент
-            XmlElement xRoot = xDoc.DocumentElement;
-            // обход всех узлов в корневом элементе
-            foreach (XmlNode xnode in xRoot)
-            {
-                // получаем атрибут name
-                if (xnode.Attributes.Count > 0)
-                {
-                    XmlNode attr = xnode.Attributes.GetNamedItem("id");
-                    if (attr != null)
-                        Console.WriteLine(attr.Value);
-                }
-                // обходим все дочерние узлы элемента user
-                foreach (XmlNode childnode in xnode.ChildNodes)
-                {
-                    // если узел - company
-                    if (childnode.Name == "fio")
-                    {
-                        Console.WriteLine("fio: {0}", childnode.InnerText);
-                    }
-                    // если узел age
-                    if (childnode.Name == "phone")
-                    {
-                        Console.WriteLine("phone: {0}", childnode.InnerText);
-                    }
-                }
-                Console.WriteLine();
-            }
+            UserElement ul = new UserElement();
+            ul.GetAll();
+            ul.GetByDep();
             Console.Read();
 
 
--- lab13/UserElement.cs
+++ lab13/UserElement.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+
+using System.Xml.Linq;
 
 namespace lab14
 {
@@ -32,14 +35,124 @@ namespace lab14
 
         public void GetAll()
         {
-            foreach(var i in li)
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("XMLlist.xml");
+            // получим корневой элемент
+            XmlElement xRoot = xDoc.DocumentElement;
+            // обход всех узлов в корневом элементе
+            foreach (XmlNode xnode in xRoot)
             {
-                Console.WriteLine(i.fio+' '+i.department +' ' + i.phone + ' ' + i.DateBegin);
+                // получаем атрибут name
+                if (xnode.Attributes.Count > 0)
+                {
+                    XmlNode attr = xnode.Attributes.GetNamedItem("id");
+                    if (attr != null)
+                        Console.WriteLine(attr.Value);
+                }
+                // обходим все дочерние узлы элемента user
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
 
+                    if (childnode.Name == "fio")
+                    {
+                        Console.WriteLine("fio: {0}", childnode.InnerText);
+                    }
 
+                    if (childnode.Name == "phone")
+                    {
+                        Console.WriteLine("phone: {0}", childnode.InnerText);
+                    }
+                    if (childnode.Name == "department")
+                    {
+                        Console.WriteLine("department: {0}", childnode.InnerText);
+                    }
+                    if (childnode.Name == "DateBegin")
+                    {
+                        Console.WriteLine("DateBegin: {0}", childnode.InnerText);
+                    }
+                }
+                Console.WriteLine();
             }
         }
 
+        public void GetByDep()
+        {
+            XmlDocument xDoc = new XmlDocument();
+            UserElement u = new UserElement();
+            xDoc.Load("XMLlist.xml");
+            // получим корневой элемент
+            XmlElement xRoot = xDoc.DocumentElement;
+            // обход всех узлов в корневом элементе
+            foreach (XmlNode xnode in xRoot)
+            {
+                // получаем атрибут name
+                if (xnode.Attributes.Count > 0)
+                {
+                    XmlNode attr = xnode.Attributes.GetNamedItem("id");
+                    if (attr != null )
+                        Console.WriteLine(attr.Value);
+                }
+                // обходим все дочерние узлы элемента user
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                   if(childnode.Name == "fio")
+                    {
+                        u.fio = childnode.InnerText;
+                    }
+                    if (childnode.Name == "department" )
+                    {
+                        u.department= childnode.InnerText;
+                    }
+                    if (childnode.Name == "phone")
+                    {
+                       u.phone= childnode.InnerText;
+                    }
+                    if (childnode.Name == "DateBegin")
+                    {
+                       //u.DateBegin = childnode.InnerText;
+                    }
+
+                    
+                    /*
+                    if (childnode.Name == "department" && childnode.InnerText == "Админестратор")
+                    {
+                        Console.WriteLine("department: {0}", childnode.InnerText);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                    
+                       if (childnode.Name == "fio" )
+                    {
+                        Console.WriteLine("fio: {0}", childnode.InnerText);
+                    }
+
+                    if (childnode.Name == "phone" )
+                    {
+                        Console.WriteLine("phone: {0}", childnode.InnerText);
+                    }
+                    
+                    if (childnode.Name == "DateBegin" )
+                    {
+                        Console.WriteLine("DateBegin: {0}", childnode.InnerText);
+                    }
+                    
+                    */
+                }
+                li.Add(u);
+
+                foreach(var i in li)
+                {
+                    if (i.department == "Админестратор")
+                    {
+                        Console.WriteLine(i.fio + i.department +i.phone +i.DateBegin);
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
 
 
     }
